7114607b555a90e9d1925c12b3ccc344acc659bb
pihari <pierre.haritz@udo.edu>
2026-03-03 14:23:32 +0100

    rm debug print statement

--- algos/safe_ppo.py
+++ algos/safe_ppo.py
@@ -378,7 +378,7 @@ class SafePPO_GBRL(PPO_GBRL):
                     break
 
                 # Fit GBRL models on the grads currently in .grad
-                print(f"Current params, grads: {self.policy.get_params()}")
+                #print(f"Current params, grads: {self.policy.get_params()}")
                 self.policy.step(policy_grad_clip=self.max_policy_grad_norm, value_grad_clip=self.max_value_grad_norm)
 
                 # Fit constraint critic
