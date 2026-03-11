f6993876d59d4c628b59cd688bcdf77836f7abc4
pihari <pierre.haritz@udo.edu>
2026-03-11 15:36:03 +0100

    temp remove of max tree threshold

--- algos/safe_ppo.py
+++ algos/safe_ppo.py
@@ -367,7 +367,8 @@ class SafePPO_GBRL(PPO_GBRL):
                                                                                th.Tensor) else rollout_data.observations
 
                 max_trees = 25000
-                max_trees_reached = self.policy.get_num_trees()[0] >= max_trees
+                max_trees_reached = False # self.policy.get_num_trees()[0] >= max_trees
+
                 if not max_trees_reached:
                     self.policy.step(observations=obs_np,
                                      policy_grad_clip=self.max_policy_grad_norm,
