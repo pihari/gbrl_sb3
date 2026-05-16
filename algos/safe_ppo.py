9f1ce1d0ea6f86b9614b17ac3335e7669baf3f69
pihari <pierre.haritz@udo.edu>
2026-05-16 14:03:21 +0200

    cost threshold mod

--- algos/safe_ppo.py
+++ algos/safe_ppo.py
@@ -202,7 +202,7 @@ class SafePPO_GBRL(PPO_GBRL):
     """
 
     def __init__(self, *args, **kwargs):
-        self.cost_threshold: float = float(kwargs.pop("cost_threshold", 25.0)) # https://arxiv.org/html/2409.01245v1
+        self.cost_threshold: float = float(kwargs.pop("cost_threshold", 100.0)) # https://arxiv.org/html/2409.01245v1
         self.use_safety_projection: bool = bool(kwargs.pop("use_safety_projection", True))
         self.use_lagrangian_rlx: bool = not self.use_safety_projection
         self.cost_value_source = kwargs.pop("cost_value_source", None)
